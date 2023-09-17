9456906ab9c65dc1397aed0eefc1fe1fac5fa57f
murari nagendra <murari0418@gmail.com>
2023-09-17 15:06:05 +0530

    mod first

--- first.py
+++ first.py
@@ -1,5 +1,10 @@
+#printing one liner
 print('hello world')
 
+#String concatination
+print("hello "+"world")
+
+#printing multiple lines
 print('''
 Trying to print multiple lines 
 with a single print statement
