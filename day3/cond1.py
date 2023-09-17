3aef175a85742f3365b68be7ab8dbc18549ac61a
murari nagendra <murari0418@gmail.com>
2023-09-17 19:14:35 +0530

    Cond1 with update

--- day3/cond1.py
+++ day3/cond1.py
@@ -2,9 +2,18 @@
 
 print('Welcome to the rollercoaster')
 height=int(input('Enter your height in cms: '))
-if height < 150:
+if height <= 150:
 	print('I am afraid that your height does not satisfy the conditions. Please try again later.')
 else:
 	print('Welcome... Please move ahead and collect your ticket')
 
 #please make sure that the indentation of the code is proper or else you will be able to get the proper output
+
+#checking if a number is even or odd
+num = int(input('Enter a number: '))
+if num % 2 == 0:
+	print('The enetered number is even')
+else:
+	print('The enetered number is odd')
+
+#Nested if statements
