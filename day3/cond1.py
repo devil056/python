4a9c5d9cbb425a1fb2e14329149b03d57135238b
murari nagendra <murari0418@gmail.com>
2023-09-17 19:05:39 +0530

    conditional statements

---
+++ day3/cond1.py
@@ -0,0 +1,7 @@
+# we will be learning the conditional statements in this stage of the python coding
+print('Welcome to the rollercoaster')
+height=int(input('Enter your height in cms: '))
+if height < 150:
+	print('I am afraid that your height does not satisfy the conditions. Please try again later.')
+else:
+	print('Welcome... Please move ahead and collect your ticket')
