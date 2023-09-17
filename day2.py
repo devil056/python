042bc95b1120409cc300e1961d4d82d328e3234d
murari nagendra <murari0418@gmail.com>
2023-09-17 15:13:59 +0530

    day2-one

---
+++ day2.py
@@ -0,0 +1,7 @@
+#indentation of code in python matters
+
+print('hello world') # this will work
+    print('Hello world') # as it is a improper indentation it will not work
+
+#the error that we get here is the unexpected indent error
+#also proper usage of the ' and " is also something that we need to keep a track of
