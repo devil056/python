6165eb94ab39bbfc1152cd9405482e7b673ce32e
murari nagendra <murari0418@gmail.com>
2023-09-18 20:41:55 +0530

    bill_pay

---
+++ day4/who_pays.py
@@ -0,0 +1,6 @@
+import random
+
+names='name1,name2,name3,name4,name5,name6,name7'
+list_name = names.split(',')
+print(list_name)
+print(f'{list_name[random.randint(0,len(list_name))-1]} will pay the bill')
