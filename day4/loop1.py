0903202734b8531f933769a88f416fe960f5474b
murari nagendra <murari0418@gmail.com>
2023-09-19 17:47:12 +0530

    loops done

---
+++ day4/loop1.py
@@ -0,0 +1,24 @@
+# for loop
+
+list1=['item1','item2','item3','item4','item5','item6']
+
+#one form of for loop
+for item in list1:
+	print(item)
+
+print('-----completed one iteration-------')
+
+#second approach
+for item in range(0,len(list1)-1):
+	print(list1[item])
+
+print('---completed 2nd iteration-------')
+
+def while_loop():
+	i=0
+	while i<=(len(list1)-1):
+		print(list1[i])
+		i+=1
+
+print('function defining and using no args')
+while_loop()
