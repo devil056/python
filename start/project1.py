0124ad29d8c7054a40399ad61605320e05de370e
murari nagendra <murari0418@gmail.com>
2023-09-19 18:12:03 +0530

    folders

---
+++ start/project1.py
@@ -0,0 +1,9 @@
+# Generate a greeting
+print('Welcome to the band generator')
+
+#Ask for the city name they are from
+city_name = input('Enter the city name that you are from: ')
+
+#ask for the pet name
+pet_name = input("Enter your pet name: ")
+print('Your bandname would be '+(city_name+" "+pet_name).title())
