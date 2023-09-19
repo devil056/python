78760f11c05806a82a7b85a4d360bfe9ffd4c6e5
murari nagendra <murari0418@gmail.com>
2023-09-19 20:18:48 +0530

    hangman first cycle

---
+++ day5/hangman.py
@@ -0,0 +1,18 @@
+import random
+
+words = ['elephant','mouse','lion']
+find = random.choice(words)
+
+word=[]
+print(find)
+for i in range(len(find)):
+	word+='_'
+
+print(word)
+while '_' in word:
+	guess = input('Enter a character: ').lower()
+	for pos in range(len(find)):
+		char = find[pos]
+		if char == guess:
+			word[pos]=guess
+	print(word)
