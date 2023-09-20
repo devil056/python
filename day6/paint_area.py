83639356a3447494679f2ae6cff4f085675fabf5
murari nagendra <murari0418@gmail.com>
2023-09-20 17:35:49 +0530

    modified

--- day6/paint_area.py
+++ day6/paint_area.py
@@ -1,6 +1,13 @@
+import math
+
 height = int(input('Enter height of the wall you wish to paint: '))
 width = int(input('Enter width of the wall you wish to paint: '))
 coverage = 5 #using the one paint we can cover 5 sq mts
 
-cans_needed = round((height * width) / coverage)
+def can_find(height,width,cover):
+	area = height*width
+	num_of_cans = math.ceil(area/cover)
+	return num_of_cans
+
+cans_needed = can_find(height=height,width=width,cover=coverage)
 print(f'you need {cans_needed} to paint the wall')
