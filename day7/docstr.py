a9d823234e075d2a28115591d2d99103cf4fc8cf
murari nagendra <murari0418@gmail.com>
2023-09-22 19:16:06 +0530

    more about func and doc strings to add references

---
+++ day7/docstr.py
@@ -0,0 +1,27 @@
+'''
+Incase if we want to create function description we can use the same multi line comment way to describe the fun
+'''
+
+def sum(a,b):
+	"""
+	Function to add two numbers
+	"""
+	return a+b
+
+
+print(sum(5,3))
+
+
+def add(n1, n2):
+  return n1 + n2
+ 
+def subtract(n1, n2):
+  return n1 - n2
+ 
+def multiply(n1, n2):
+  return n1 * n2
+ 
+def divide(n1, n2):
+  return n1 / n2
+ 
+print(add(2, multiply(5, divide(8, 4))))
