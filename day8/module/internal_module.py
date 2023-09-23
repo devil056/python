e5f497fbe2c453e9cd7b16613029f3ca4aa7b6ba
murari nagendra <murari0418@gmail.com>
2023-09-23 22:51:20 +0530

    object oriented prgms

---
+++ day8/module/internal_module.py
@@ -0,0 +1,3 @@
+#sample value
+
+some_value = 'This can be of any data type'
---
+++ day8/module/main.py
@@ -0,0 +1,8 @@
+import internal_module
+
+print(internal_module.some_value)
+
+from turtle import Turtle
+
+panda = Turtle()
+print(panda)
