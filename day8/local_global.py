db42e9771704021ae15d00f9533607d5b00e15ab
murari nagendra <murari0418@gmail.com>
2023-09-23 21:48:56 +0530

    local variables and global variables

--- day8/local_global.py
+++ day8/local_global.py
@@ -7,3 +7,21 @@ def drink_potion():
 
 drink_potion()
 print(player_health)
+
+#having a global and we can refer them in func using global keywords
+
+print('before the initial update')
+def print_var():
+    global player_health 
+    player_health+=1 #this is one way to do
+    print(player_health)
+
+print_var()
+
+def increase_val():
+    val = player_health
+    return val+5 
+
+print('before the second update')
+player_health = increase_val()
+print(player_health)
