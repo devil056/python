e60d8f8768cfdde21a7f475c88b78da39adfb9c5
murari nagendra <murari0418@gmail.com>
2023-09-25 19:10:36 +0530

    turtle

--- day9/main.py
+++ day9/main.py
@@ -1,4 +1,4 @@
-import day9.mod1 as mod1
+import mod1
 
 print(mod1.some_var)
 
@@ -6,6 +6,9 @@ from turtle import Turtle,Screen
 
 timmy = Turtle()
 print(timmy)
-
+timmy.shape("turtle")
+timmy.color("blue")
+timmy.forward(100)
 my_screen = Screen()
 print(my_screen.canvheight)
+my_screen.exitonclick()
