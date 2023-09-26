15667840cc45ab5e96319f10916aefdeb7d43734
murari nagendra <murari0418@gmail.com>
2023-09-27 00:23:51 +0530

    Class & def

---
+++ day10/main.py
@@ -0,0 +1,25 @@
+class MoneyMachine:
+    def report():
+        print('Report')
+
+
+
+class CoffeeMaker:
+    def report():
+        print('Report from the coffee maker')
+
+    def is_resource_enuf():
+        print('Method to check if the resource is enough')
+
+    def make_coffee():
+        print('In the process of making coffee')
+
+class Menu:
+    def get_items():
+        print('Items from the menu class')
+    
+    def find_drink(order_num):
+        print('Checking the drink based on the order num')
+
+money_machine = MoneyMachine()
+money_machine.report()
