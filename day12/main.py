1532fc4e283768749daac3032633053d056d0e27
murari nagendra <murari0418@gmail.com>
2023-09-30 15:59:43 +0530

    quiz app

---
+++ day12/main.py
@@ -0,0 +1,11 @@
+from question_model import Question
+from data import question_data
+
+question_bank=[]
+for item in question_data:
+	question_bank.append(Question(item['text'],item['answer']))
+
+
+print(question_bank)
+for item in question_bank:
+	print(f'{item.question} Ans: {item.answer}')
