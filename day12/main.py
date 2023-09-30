cbd4379dcc19f5825beb55e656fa8e700e01524f
murari nagendra <murari0418@gmail.com>
2023-09-30 21:40:02 +0530

    quiz brain

--- day12/main.py
+++ day12/main.py
@@ -1,7 +1,9 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank=[]
 for item in question_data:
 	question_bank.append(Question(item['text'],item['answer']))
 
+qb = QuizBrain(question_bank)
---
+++ day12/quiz_brain.py
@@ -0,0 +1,4 @@
+class QuizBrain:
+	def __init__(self,que_list):
+		self.que_num=0
+		self.que_list=que_list
