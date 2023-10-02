7239817b8143d3459b8b7392f249186c7c4830a5
murari nagendra <murari0418@gmail.com>
2023-10-02 20:29:09 +0530

    working ex

--- day13/tut.py
+++ day13/tut.py
@@ -1,9 +1,12 @@
-import turtle
+from turtle import Turtle, Screen
 import random
 
-t = turtle.Turtle()
+t = Turtle()
+screen=Screen()
+screen.colormode(255)
+def color_generator():
+	return (random.randint(0,255),random.randint(0,255),random.randint(0,255))
 
-colors = ['medium spring green','dark slate gray','light steel blue','medium purple','orchid','goldenrod','salmon']
 directions= [0,90,180,270]
 t.pensize(10)
 # for i in range(15):
@@ -12,9 +15,9 @@ t.pensize(10)
 # 	t.forward(10)
 # 	t.pendown()
 
-for i in range(200):
-	t.color(random.choice(colors))
+for i in range(20):
+	t.color(color_generator())
 	t.forward(20)
 	t.setheading(random.choice(directions))
 
-turtle.exitonclick()
+screen.exitonclick()
