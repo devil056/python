32a72f637f71f044ef489bc1e67edeef2e6489ce
murari nagendra <murari0418@gmail.com>
2023-10-02 20:41:51 +0530

    circles are also added

--- day13/tut.py
+++ day13/tut.py
@@ -8,16 +8,22 @@ def color_generator():
 	return (random.randint(0,255),random.randint(0,255),random.randint(0,255))
 
 directions= [0,90,180,270]
-t.pensize(10)
+# t.pensize(10)
 # for i in range(15):
 # 	t.forward(10)
 # 	t.penup()
 # 	t.forward(10)
 # 	t.pendown()
 
-for i in range(20):
+# for i in range(200):
+# 	t.color(color_generator())
+# 	t.forward(20)
+# 	t.setheading(random.choice(directions))
+t.pensize(2)
+t.speed('fastest')
+for i in range(100):
 	t.color(color_generator())
-	t.forward(20)
-	t.setheading(random.choice(directions))
+	t.circle(100)
+	t.setheading(t.heading()+10)
 
 screen.exitonclick()
