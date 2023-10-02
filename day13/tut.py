1b2b00aa41d59217d2344dc0344c3c1a74d011ae
murari nagendra <murari0418@gmail.com>
2023-10-02 19:42:06 +0530

    pensize

--- day13/tut.py
+++ day13/tut.py
@@ -5,14 +5,14 @@ t = turtle.Turtle()
 
 colors = ['medium spring green','dark slate gray','light steel blue','medium purple','orchid','goldenrod','salmon']
 directions= [0,90,180,270]
-
+t.pensize(10)
 # for i in range(15):
 # 	t.forward(10)
 # 	t.penup()
 # 	t.forward(10)
 # 	t.pendown()
 
-for i in range(150):
+for i in range(200):
 	t.color(random.choice(colors))
 	t.forward(20)
 	t.setheading(random.choice(directions))
